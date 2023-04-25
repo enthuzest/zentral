9c943005f4ab8d652cf6c9b9affcb0fadcde9397
faraz ahmad siddiqui <faraz841@gmail.com>
2023-04-25 15:04:01 +1000

    updated property

--- modules/function-app/main.tf
+++ modules/function-app/main.tf
@@ -20,13 +20,15 @@ resource "azurerm_function_app" "primary" {
   storage_account_name       = azurerm_storage_account.sa.name
   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
   app_settings               = var.app_settings
-  identity = {
+  version = var.func_version
+
+  identity {
     type = "SystemAssigned"
   }
-  site_config = {
-        always_on = true
+  site_config {
+    always_on = true
+    dotnet_framework_version = var.dotnet_framework_version
   }
-  dotnet_framework_version = var.dotnet_framework_version
-  version = var.func_version
+
   tags = var.tags
 }
