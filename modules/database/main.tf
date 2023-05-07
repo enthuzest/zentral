c5468177221ea51957d631ff150c62b25853f653
faraz ahmad siddiqui <faraz841@gmail.com>
2023-05-07 16:43:40 +1000

    updated db max size

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_mssql_database" "test" {
   server_id    = var.server_id
   collation    = "SQL_Latin1_General_CP1_CI_AS"
   license_type = "LicenseIncluded"
-  max_size_gb  = 4
+  max_size_gb  = 1
   sku_name     = "Basic"
   tags         = var.tags
 }
