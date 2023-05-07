221b218c63da2b634cfcae0fd4de9ffc8c9e5826
faraz ahmad siddiqui <faraz841@gmail.com>
2023-05-07 11:48:54 +1000

    added base module

---
+++ modules/database/main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_mssql_database" "test" {
+  name           = var.database_name
+  server_id      = var.server_id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  sku_name       = "Basic"
+  tags = var.tags
+}
