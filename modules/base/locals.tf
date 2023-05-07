bacb9b28f0865b74db370591db988eec0fcef77f
faraz ahmad siddiqui <faraz841@gmail.com>
2023-05-07 12:12:36 +1000

    updated type

--- modules/base/locals.tf
+++ modules/base/locals.tf
@@ -1,6 +1,8 @@
 locals {
-  trusted_ip_ranges = {
-    name    = "homeIP"
-    network = "136.23.11.65"
-  }
+  trusted_ip_ranges = [
+    {
+      "name"    = "homeIP"
+      "network" = "136.23.11.65"
+    }
+  ]
 }
